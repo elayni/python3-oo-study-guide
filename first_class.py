1d651484661433b4ddf34bb7529542eb68593a10
Elayni <elaynienoly@gmail.com>
2021-03-09 08:50:02 -0300

    add first class

---
+++ first_class.py
@@ -0,0 +1,4 @@
+# class name must start with letter or underscore
+# can only be comprised of letters, underscores or numbers
+class MyFirstClass:  # Pep 8 recommends that classes should ne named using CapWord
+    pass
